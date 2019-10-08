f3748b67d938af65acea4e6b8bad9b61e0c7d791
Robert Widmann <devteam.codafi@gmail.com>
2019-10-08 10:59:01 -0700

    XXX

--- lib/Sema/MiscDiagnostics.cpp
+++ lib/Sema/MiscDiagnostics.cpp
@@ -780,8 +780,8 @@ static void diagSyntacticUseRestrictions(const Expr *E, const DeclContext *DC,
         return;
 
       if (!DC->isTestContext()) {
-        TC.diagnose(DRE->getLoc(), diag::test_func_in_non_test_context,
-                    DRE->getDecl()->getBaseName().getIdentifier());
+        Ctx.Diags.diagnose(DRE->getLoc(), diag::test_func_in_non_test_context,
+                           DRE->getDecl()->getBaseName().getIdentifier());
       }
     }
     
