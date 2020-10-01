71c864d0632e359e869888dd8e7932c42ed4ce60
Azoy <aalonso128@outlook.com>
2020-10-01 18:47:30 -0400

    [AST] Support conditional conformance for parameterized extensions

--- lib/AST/ProtocolConformance.cpp
+++ lib/AST/ProtocolConformance.cpp
@@ -538,10 +538,10 @@ ConditionalRequirementsRequest::evaluate(Evaluator &evaluator,
   // The extension signature should be a superset of the type signature, meaning
   // every thing in the type signature either is included too or is implied by
   // something else. The most important bit is having the same type
-  // parameters. (NB. if/when Swift gets parameterized extensions, this needs to
-  // change.)
-  assert(typeSig.getCanonicalSignature().getGenericParams() ==
-         extensionSig.getCanonicalSignature().getGenericParams());
+  // parameters. If the extension is parameterized however, this is untrue.
+  assert(ext->isParameterized() ||
+         (typeSig.getCanonicalSignature().getGenericParams() ==
+         extensionSig.getCanonicalSignature().getGenericParams()));
 
   // Find the requirements in the extension that aren't proved by the original
   // type, these are the ones that make the conformance conditional.
